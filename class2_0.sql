9c83c6775d78f83f0c878ca775a4328345a17e56
AlexaneLG <leguern.a@laposte.net>
2017-05-14 00:14:56 +0200

    Déconnexion

--- class2_0.sql
+++ class2_0.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 08, 2017 at 09:23 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 7.0.13
+-- Client :  127.0.0.1
+-- Généré le :  Sam 13 Mai 2017 à 22:13
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,77 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `class2.0`
+-- Base de données :  `class2_0`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `category`
+--
+
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL,
+  `content` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `course`
+--
+
+CREATE TABLE `course` (
+  `id_course` int(11) NOT NULL,
+  `chapter` int(11) NOT NULL,
+  `subject` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id_feedback` int(11) NOT NULL,
+  `id_professor` int(11) NOT NULL,
+  `id_student` int(11) NOT NULL,
+  `mark` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `meeting`
+--
+
+CREATE TABLE `meeting` (
+  `id_meeting` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `hour` datetime NOT NULL,
+  `numhours` datetime NOT NULL,
+  `id_prof` int(11) NOT NULL,
+  `id_student` int(11) NOT NULL,
+  `id_course` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `professor_meeting`
+--
+
+CREATE TABLE `professor_meeting` (
+  `id_professor_meeting` int(11) NOT NULL,
+  `id_meeting` int(11) NOT NULL,
+  `id_professor` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
 --
 
 CREATE TABLE `user` (
@@ -32,36 +96,125 @@ CREATE TABLE `user` (
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `account` varchar(255) NOT NULL
+  `account` varchar(255) NOT NULL,
+  `mark` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `account`) VALUES
-(7, 'Olivier', 'LEUNG', 'olivierleung@hotmail.fr', '123456789', 'user'),
-(8, 'Alex', 'LEUNG', 'aleung@hotmail.fr', '123456789', 'on');
+INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `account`, `mark`) VALUES
+(7, 'Olivier', 'LEUNG', 'olivierleung@hotmail.fr', '123456789', 'user', 0),
+(8, 'Alex', 'LEUNG', 'aleung@hotmail.fr', '123456789', 'on', 0),
+(9, 'Alexane', 'LE GUERN', 'alexane.lgrn@gmail.com', 'km', 'user', 0);
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Structure de la table `user_category`
+--
+
+CREATE TABLE `user_category` (
+  `id_user_category` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `user`
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
+
+--
+-- Index pour la table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id_course`);
+
+--
+-- Index pour la table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id_feedback`),
+  ADD KEY `id_professor` (`id_professor`),
+  ADD KEY `id_student` (`id_student`);
+
+--
+-- Index pour la table `meeting`
+--
+ALTER TABLE `meeting`
+  ADD PRIMARY KEY (`id_meeting`),
+  ADD KEY `id_prof` (`id_prof`),
+  ADD KEY `id_student` (`id_student`),
+  ADD KEY `id_course` (`id_course`);
+
+--
+-- Index pour la table `professor_meeting`
+--
+ALTER TABLE `professor_meeting`
+  ADD PRIMARY KEY (`id_professor_meeting`),
+  ADD KEY `id_meeting` (`id_meeting`),
+  ADD KEY `id_professor` (`id_professor`);
+
+--
+-- Index pour la table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Index pour la table `user_category`
+--
+ALTER TABLE `user_category`
+  ADD PRIMARY KEY (`id_user_category`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_category` (`id_category`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT pour la table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id_course` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id_feedback` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `meeting`
+--
+ALTER TABLE `meeting`
+  MODIFY `id_meeting` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `professor_meeting`
+--
+ALTER TABLE `professor_meeting`
+  MODIFY `id_professor_meeting` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT pour la table `user_category`
+--
+ALTER TABLE `user_category`
+  MODIFY `id_user_category` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
